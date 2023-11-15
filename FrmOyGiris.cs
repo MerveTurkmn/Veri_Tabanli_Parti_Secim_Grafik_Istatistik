2016e11767b9752df343d3240ec281d99f1a5d2e
Merve Türkmen <merve.turkmenn@outlook.com>
2023-11-15 16:42:13 +0300

    Add project files.

---
+++ FrmOyGiris.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Veri_Tabanlı_Parti_Seçim_Grafik_İstatistik
+{
+    public partial class FrmOyGiris : Form
+    {
+        public FrmOyGiris()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=LAPTOP-HLMQ9PKG;Initial Catalog=DBSECIMPROJE;Integrated Security=True");
+        private void BtnOyGiris_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into TBLILCE (ILCEAD,APARTI,BPARTI,CPARTI,DPARTI,EPARTI) VALUES (@P1,@P2,@P3,@P4,@P5,@P6)", baglanti);
+            komut.Parameters.AddWithValue("P1", TxtilceAd.Text);
+            komut.Parameters.AddWithValue("P2", TxtA.Text);
+            komut.Parameters.AddWithValue("P3", TxtB.Text);
+            komut.Parameters.AddWithValue("P4", TxtC.Text);
+            komut.Parameters.AddWithValue("P5", TxtD.Text);
+            komut.Parameters.AddWithValue("P6", TxtE.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Oy Girişi Gerçekleşti.");
+
+        }
+
+        private void BtnGrafikler_Click(object sender, EventArgs e)
+        {
+            FrmGrafikler fr = new FrmGrafikler();
+            fr.Show();
+        }
+    }
+}
